e8ae74ee5959e0c24676dcf67eb8c53806a658de
niranjanrai <rainiranjan1998@gmail.com>
2022-10-02 00:39:07 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+// Dynamic Scoping
+// let movie = "tamasha";
+
+// function otherMovies(movieName) {
+//   console.log(movie, movieName);
+// }
+
+// function otherOtherMovies() {
+//   let movie = "yeh jawani hai deewani";
+//   otherMovies("brahmastra");
+// }
+
+// otherOtherMovies();
+
+function otherMovie(movieName) {
+  console.log(this.movie, movieName);
+}
+
+function otherOtherMovie() {
+  let myContext = {
+    movie: "Tamasha",
+  };
+  otherMovie.call(myContext, "is coming");
+}
+
+otherOtherMovie();
