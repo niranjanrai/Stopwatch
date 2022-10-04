523aeb519c638a90856efdb54ebf87ff059b3dd4
niranjanrai <rainiranjan1998@gmail.com>
2022-10-04 10:10:01 +0530

    first commit

--- app.js
+++ app.js
@@ -12,15 +12,15 @@
 
 // otherOtherMovies();
 
-// function otherMovie(movieName) {
-//   console.log(this.movie, movieName);
-// }
+function otherMovie(movieName) {
+  console.log(this.movie, movieName);
+}
 
-// function otherOtherMovie() {
-//   let myContext = {
-//     movie: "Tamasha",
-//   };
-//   otherMovie.call(myContext, "is coming");
-// }
+function otherOtherMovie() {
+  let myContext = {
+    movie: "Tamasha",
+  };
+  otherMovie.call(myContext, "is coming");
+}
 
-// otherOtherMovie();
+otherOtherMovie();
